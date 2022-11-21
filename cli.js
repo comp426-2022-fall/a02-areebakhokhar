093efbea984705c0b202d934c0466e5659d35a2a
Areeba Khokhar <areebak@email.unc.edu>
2022-11-20 22:17:37 -0500

    galoshes

--- cli.js
+++ cli.js
@@ -34,6 +34,12 @@ console.log(data.precipitation);
 
     const days = args.d
 
+    if (data.daily.precipitation_hours[days] != 0.0) {
+        console.log("You might need your galoshes");
+    } else{
+        console.log("You will not need your galoshes");
+    }
+
     if (days == 0) {
       console.log("today.")
     } else if (days > 1) {
